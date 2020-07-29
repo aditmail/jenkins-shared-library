b8dbbd5375c7b96bfce0ca2b40d772b6f077f1bb
aditmail <aditmail321@gmail.com>
2020-07-29 17:45:46 +0700

    add another method

--- vars/testCall.groovy
+++ vars/testCall.groovy
@@ -1,14 +1,16 @@
 def call(Map config = [:], Closure body) {
     node {
-        git url: "urls..."
+        //git url: "urls..."
 
         stage("test") {
-
+            echo "test"
         }
 
         stage("deploy") {
             if (config.deploy) {
-                //....
+                echo "config ${config.deploy}"
+            }else{
+                echo "not deploy.."
             }
         }
 
