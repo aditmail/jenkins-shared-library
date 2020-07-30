22089782c55c6506443f44a29d6711da40bae3b0
aditmail <aditmail321@gmail.com>
2020-07-30 12:19:32 +0700

    test lagi

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -3,43 +3,57 @@ import main.groovy.example.Constant
 def call(Map config = [:]) {
     switch (config.utilities) {
         case 'dateTime':
-            dateTime()
+            /* Get TimeStamp */
+            return new Date().format('dd/MM/yyyy HH:mm:ss')
             break
 
         case 'inputEmail':
-            inputEmail()
+            input(
+                    message: "${Constant.INPUT_EMAIL_MSG}",
+                    ok: "$Constant.INSERT_BTN",
+                    parameters: [
+                            string(
+                                    defaultValue: "$Constant.EMAIL_EXAMPLE",
+                                    description: "$Constant.DESCRIPTION_EMAIL_MSG",
+                                    name: 'inputEmailTo',
+                                    trim: true
+                            )
+                    ]
+            )
             break
 
         case 'validateEmail':
-            validateEmail(config.params = null)
+            /* Check Email Validation */
+            def emailAddress = config.params
+            return (emailAddress == null || emailAddress == "" || emailAddress == "example@email.com")
             break
+
         default: break
     }
-
 }
 
-/** Get TimeStamp **/
-static def String dateTime() {
-    return new Date().format('dd/MM/yyyy HH:mm:ss')
-}
-
-/** Set Email Address **/
-def inputEmail() {
-    input(
-            message: "${Constant.INPUT_EMAIL_MSG}",
-            ok: "$Constant.INSERT_BTN",
-            parameters: [
-                    string(
-                            defaultValue: "$Constant.EMAIL_EXAMPLE",
-                            description: "$Constant.DESCRIPTION_EMAIL_MSG",
-                            name: 'inputEmailTo',
-                            trim: true
-                    )
-            ]
-    )
-}
-
-/** Check Email Validation **/
-static def Boolean validateEmail(emailAddress) {
-    return (emailAddress == null || emailAddress == "" || emailAddress == "example@email.com")
-}
+///** Get TimeStamp **/
+//static def String dateTime() {
+//    return new Date().format('dd/MM/yyyy HH:mm:ss')
+//}
+//
+///** Set Email Address **/
+//def inputEmail() {
+//    input(
+//            message: "${Constant.INPUT_EMAIL_MSG}",
+//            ok: "$Constant.INSERT_BTN",
+//            parameters: [
+//                    string(
+//                            defaultValue: "$Constant.EMAIL_EXAMPLE",
+//                            description: "$Constant.DESCRIPTION_EMAIL_MSG",
+//                            name: 'inputEmailTo',
+//                            trim: true
+//                    )
+//            ]
+//    )
+//}
+//
+///** Check Email Validation **/
+//static def Boolean validateEmail(emailAddress) {
+//    return (emailAddress == null || emailAddress == "" || emailAddress == "example@email.com")
+//}
