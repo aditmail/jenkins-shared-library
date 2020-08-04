ba0127b411177758c0bfd7fb4bfacbe3a5ce796b
aditmail <aditmail321@gmail.com>
2020-08-04 10:29:54 +0700

    test JenkinsFile inside JenkinsJob

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -2,10 +2,13 @@ import main.groovy.example.Constant
 
 import java.util.regex.Pattern
 
-def call(Map config = [:]) {
+/**
+ * Unused Method.. This used before
+ * I know the using of method calling **/
+/*def call(Map config = [:]) {
     switch (config.utilities) {
         case 'dateTime':
-            /* Get TimeStamp */
+            *//* Get TimeStamp *//*
             return new Date().format('dd/MM/yyyy HH:mm:ss')
             break
 
@@ -25,7 +28,7 @@ def call(Map config = [:]) {
             break
 
         case 'validateEmail':
-            /* Check Email Validation */
+            *//* Check Email Validation *//*
             def emailAddress = config.params
             return (emailAddress == null || emailAddress == "" || emailAddress == "example@email.com")
             break
@@ -40,22 +43,6 @@ def call(Map config = [:]) {
 
         default: break
     }
-}
-
-/** Generate JUnit Test Result **/
-/*def generateJUnit(location = "") {
-    echo "$Constant.GENERATE_JUNIT_REPORT"
-    junit testResults: location
-}*/
-
-/** Generate Checkstyle Reports **/
-/*def generateCheckstyle(location = "") {
-    echo "$Constant.GENERATE_CHECKSTYLE_REPORT"
-    recordIssues(
-            tools: [
-                    checkStyle(pattern: location)
-            ]
-    )
 }*/
 
 /** Get TimeStamp **/
